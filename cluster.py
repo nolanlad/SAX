38e73f03a475d6c1687a4179a29d1d73a0d3c340
nolanlad <37968513+nolanlad@users.noreply.github.com>
2020-01-21 15:59:25 -0800

    clustering

---
+++ cluster.py
@@ -0,0 +1,13 @@
+from SAX import *
+from total_sax_cluster import get_heatmaps
+from sklearn.cluster import KMeans
+
+NBINS = 5
+space = EqualBinSpace(NBINS)
+
+v1,v2 = read_ae_file2("./minicomp-dat/2-4.txt")
+
+X = get_heatmaps(v1,v2,space)
+
+kmeans = KMeans(n_clusters=5,random_state=0).fit(X)
+
